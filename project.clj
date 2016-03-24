762675e6d250b70389769c2e5eac0ab5ccb71a9b
Alexander Kiel <alexanderkiel@gmx.net>
2016-03-24 22:17:46 +0100

    Sort Dependencies

--- project.clj
+++ project.clj
@@ -4,24 +4,24 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/tools.reader "0.10.0"]
-                 [prismatic/plumbing "0.5.2"]
-                 [prismatic/schema "1.0.4"]
-                 [http-kit "2.1.18"]
-                 [bidi "1.25.0"]
-                 [com.cognitect/transit-clj "0.8.285"]
-                 [clj-time "0.11.0"]
-                 [org.slf4j/slf4j-api "1.7.7"]
+  :dependencies [[bidi "1.25.0"]
                  [ch.qos.logback/logback-classic "1.1.2"]
+                 [clj-time "0.11.0"]
+                 [com.cognitect/transit-clj "0.8.285"]
                  [com.novemberain/langohr "3.5.1"
                   :exclusions [clj-http cheshire]]
-                 [javax.servlet/servlet-api "2.5"]
                  [com.taoensso/carmine "2.12.2"]
                  [com.stuartsierra/component "0.3.0"]
+                 [danlentz/clj-uuid "0.1.6"]
                  [environ "1.0.1"]
-                 [danlentz/clj-uuid "0.1.6"]]
+                 [http-kit "2.1.18"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.reader "0.10.0"]
+                 [org.slf4j/slf4j-api "1.7.7"]
+                 [prismatic/plumbing "0.5.2"]
+                 [prismatic/schema "1.0.4"]]
 
   :profiles {:dev
              {:source-paths ["dev"]
