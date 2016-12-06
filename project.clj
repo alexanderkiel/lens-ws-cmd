48971d180afdd70603ded9843d8596e1ae4a0992
Alexander Kiel <alexanderkiel@gmx.net>
2016-12-06 20:34:48 +0100

    Release v0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lens-ws-cmd "0.2"
+(defproject lens-ws-cmd "0.3"
   :description "Lens Command Webservice"
   :url "https://github.com/alexanderkiel/lens-ws-cmd"
   :license {:name "Eclipse Public License"
