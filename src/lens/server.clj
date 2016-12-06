9a88546076c100427bcc21ec9bf1140292e03528
Alexander Kiel <alexanderkiel@gmx.net>
2016-12-06 20:03:49 +0100

    Increase Max Body Size to 256 MB

--- src/lens/server.clj
+++ src/lens/server.clj
@@ -9,7 +9,7 @@
   (start [server]
     (info {:component "Server" :msg (str "Start server on port " port)})
     (let [handler (app server)
-          opts (assoc server :max-body (* 100 1024 1024))]
+          opts (assoc server :max-body (* 256 1024 1024))]
       (assoc server :stop-fn (run-server handler opts))))
   (stop [server]
     (info {:component "Server" :msg "Stop server"})
